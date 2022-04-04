1ed5f4d2f7a2bfd9324dc82dd4675f9f4ec3808e
parkjung1129 <dlwfp94@gmail.com>
2022-04-04 14:22:35 +0900

    :label: React Route Dom 사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Abort from "./pages/About";
+import NotFound from "./pages/Errors/NotFound";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    <Route path="/home" element={<Home/>}></Route>
+                    <Route path="/blog/*" element={<Blog/>}></Route>
+                    <Route path="/abort" element={<Abort/>}></Route>
+                    <Route path="*" element={<NotFound/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Blog/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+const Blog = () => {
+    return (
+        <>
+            <h2> Blog Page</h2>
+        </>
+    )
+};
+
+export default Blog;
